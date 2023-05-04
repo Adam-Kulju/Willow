49fd5d92b960c353fcd29786b936cb404f229b72
Adam Kulju <85965318+Adam-Kulju@users.noreply.github.com>
2023-05-04 16:20:53 -0500

    Update willow.c
    Bench: 7964428

--- src/willow.c
+++ src/willow.c
@@ -1770,7 +1770,7 @@ int quiesce(struct board_info *board, int alpha, int beta, int depth, int depthl
         if (stand_pat > alpha){
             alpha = stand_pat;
         }
-        futility = stand_pat + 90;
+        futility = stand_pat + 60;
 
     }
 
@@ -1802,16 +1802,11 @@ int quiesce(struct board_info *board, int alpha, int beta, int depth, int depthl
             if (list[i].eval < 1000200){
                 break;
             }
-           /* if (futility + VALUES2[(board->board[list[i].move.move & 0xFF]>>1) - 1] <= alpha && !list[i].move.flags){
-                    bestscore = MAX(bestscore, futility + VALUES2[board->board[list[i].move.move & 0xFF]>>1]);
+            if (futility + VALUES2[(board->board[list[i].move.move & 0xFF]>>1) - 1] <= alpha && !list[i].move.flags){
+                    bestscore = MAX(bestscore, futility + VALUES2[(board->board[list[i].move.move & 0xFF]>>1) - 1]);
                     i++;
                     continue;
-                }*/
-            if (futility <= alpha && !static_exchange_evaluation(board, list[i].move, color, 1)){
-                bestscore = MAX(bestscore, futility);
-                i++;
-                continue;
-            }
+                }
 
         }
         struct board_info board2 = *board;
