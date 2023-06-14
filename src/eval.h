f0cf9b7b521ba235d35e0d205b0005744089766e
Adam Kulju <85965318+Adam-Kulju@users.noreply.github.com>
2023-06-14 09:14:46 -0500

    Update eval.h

--- src/eval.h
+++ src/eval.h
@@ -544,25 +544,6 @@ void pst(struct board_info *board, int phase, int *mgscore, int *egscore ) // A 
         }
     }
 
-    /*if (phase > 16)
-    {
-        // If we're still early in the game, calculate space.
-        int weight0 = 0, weight1 = 0;
-
-        for (int i = 0; i < 5; i++) // One of the weights is dependent on the amount of material on the board...
-        {
-            weight0 += board->pnbrqcount[WHITE][i];
-            weight1 += board->pnbrqcount[BLACK][i];
-        }
-        blockedpawns = MIN(blockedpawns, 9); // and the other one is on how blocked up the position is (up to a limit).
-
-        weight0 = MAX(0, weight0 - 3 + 1 + blockedpawns);
-        weight1 = MAX(0, weight1 - 3 + 1 + blockedpawns);
-
-        int space = ((((spacew * weight0 * weight0) >> 4) - ((spaceb * weight1 * weight1) >> 4))) >> 1;
-        *mgscore += space * 5 / 10, *egscore  += space * 5 / 10; // This final weight is what the tuner spits out after everything else is calculated as a multiplier.
-    }*/
-
     if (attackers[BLACK] > 1)
     {
         king_attack_count[BLACK] = MIN(king_attack_count[BLACK], 99);
