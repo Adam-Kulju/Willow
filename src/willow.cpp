8491307959828301e29cb6827f95ff62eca6d5ff
Adam Kulju <85965318+Adam-Kulju@users.noreply.github.com>
2023-06-18 12:24:27 -0500

    update version name

--- src/willow.cpp
+++ src/willow.cpp
@@ -103,7 +103,7 @@ int com_uci(struct board_info *board, struct movelist *movelst, int *key, bool *
 
     if (!strcmp(command, "uci"))
     {
-        printf("id name Willow 2.8\n");
+        printf("id name Willow 2.9\n");
         printf("id author Adam Kulju\n");
         // send options
         printf("option name Hash type spin default 32 min 1 max 1028\n");
