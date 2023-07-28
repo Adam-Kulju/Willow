b7983e578b7fda10cd5ff0f3829ca7edd2ba1da3
Adam Kulju <85965318+Adam-Kulju@users.noreply.github.com>
2023-07-27 23:36:08 -0500

    an alex-style fp

--- src/search.h
+++ src/search.h
@@ -425,7 +425,7 @@ int alphabeta(struct board_info *board, struct movelist *movelst, int *key, int 
         if (depth > 0 && !iscap && !ispv)
         {
             int newdepth = MAX(depthleft - 1 - LMRTABLE[depthleft-1][betacount] + improving, 0);
-            int futility_move_count = (3 + newdepth * newdepth / (1 + (!improving)));
+            int futility_move_count = (3 + depthleft * depthleft / (1 + (!improving)));
             // Late Move Pruning (LMP): at high depths, we can just not search quiet moves after a while.
             // They are very unlikely to be unavoidable even if they are good and it saves time.
             if (newdepth < 4)
@@ -436,7 +436,7 @@ int alphabeta(struct board_info *board, struct movelist *movelst, int *key, int 
                 }
             }
             // Futility Pruning: If our position is bad enough, only search captures after this one.
-            if ((newdepth < 6 && !incheck && list[i].eval < 1000200 && evl + 80 + 90 * (depthleft) < alpha))
+            if ((!incheck && newdepth < 10 && list[i].eval < 1000200 && evl + 100 + 150 * (depthleft) < alpha))
             {
                 quietsprune = true;
             }
