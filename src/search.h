367b2823df3ffc680c9e32fda75ef913443c57e8
Adam Kulju <85965318+Adam-Kulju@users.noreply.github.com>
2023-09-05 13:39:50 -0500

    fix SE tt depth and stop grabbing directly from the TT
    Bench: 18800846

--- src/search.h
+++ src/search.h
@@ -50,11 +50,12 @@ int quiesce(struct board_info *board, int alpha, int beta, int depth, int depthl
     }
     int evl = 0;
     char type;
-    if (thread_info->CURRENTPOS == TT[(thread_info->CURRENTPOS) & (_mask)].zobrist_key)
+    struct ttentry entry = TT[(thread_info->CURRENTPOS) & (_mask)];
+    if (thread_info->CURRENTPOS == entry.zobrist_key)
     // Probe the transposition table. If we got an hit we may be able to cut of immediately, if not it may stil be useful for move ordering.
     {
-        type = TT[(thread_info->CURRENTPOS) & (_mask)].type;
-        evl = TT[(thread_info->CURRENTPOS) & (_mask)].eval;
+        type = entry.type;
+        evl = entry.eval;
     }
     else
     {
@@ -96,7 +97,7 @@ int quiesce(struct board_info *board, int alpha, int beta, int depth, int depthl
         stand_pat = eval(board, color, thread_info);
         if (type == 3 || (type == UBound && ttscore < stand_pat) || (type == LBound && ttscore > stand_pat)) // Use the evaluation from the transposition table as it is more accurate than the static evaluation.
         {
-            stand_pat = TT[(thread_info->CURRENTPOS) & (_mask)].eval;
+            stand_pat = entry.eval;
         }
     }
 
@@ -120,7 +121,7 @@ int quiesce(struct board_info *board, int alpha, int beta, int depth, int depthl
     struct list list[LISTSIZE];
     int listlen = movegen(board, list, color, incheck);
 
-    movescore(board, list, 99, color, type, nullmove, listlen, -108, thread_info);
+    movescore(board, list, 99, color, type, nullmove, listlen, -108, thread_info, entry);
     // score the moves
 
     struct move bestmove = nullmove;
@@ -244,10 +245,11 @@ int alphabeta(struct board_info *board, struct movelist *movelst, int *key, int 
     bool singularsearch = !ismatch(excludedmove, nullmove);
 
     char type;
-    if (!singularsearch && thread_info->CURRENTPOS == TT[(thread_info->CURRENTPOS) & (_mask)].zobrist_key) // Probe the transposition table.
+    ttentry entry = TT[(thread_info->CURRENTPOS) & (_mask)];
+    if (!singularsearch && thread_info->CURRENTPOS == entry.zobrist_key) // Probe the transposition table.
     {
-        type = TT[(thread_info->CURRENTPOS) & (_mask)].type;
-        evl = TT[(thread_info->CURRENTPOS) & (_mask)].eval;
+        type = entry.type;
+        evl = entry.eval;
     }
     else
     {
@@ -259,7 +261,7 @@ int alphabeta(struct board_info *board, struct movelist *movelst, int *key, int 
 
     bool ispv = (beta != alpha + 1); // Are we in a PV (i.e. likely best line) node? This affects what type of pruning we can do.
 
-    if (!ispv && type != None && TT[(thread_info->CURRENTPOS) & (_mask)].depth >= depthleft) // Check to see if we can cutoff
+    if (!ispv && type != None && entry.depth >= depthleft) // Check to see if we can cutoff
     {
         if (type == Exact)
         {
@@ -313,7 +315,7 @@ int alphabeta(struct board_info *board, struct movelist *movelst, int *key, int 
 
     if (type == Exact || (type == UBound && ttscore < evl) || (type == LBound && ttscore > evl)) // Use the evaluation from the transposition table as it is more accurate than the static evaluation.
     {
-        evl = TT[(thread_info->CURRENTPOS) & (_mask)].eval;
+        evl = entry.eval;
     }
 
     // Reverse Futility Pruning: If our position is so good that we don't need to move to beat beta + some margin, we cut off early.
@@ -383,7 +385,7 @@ int alphabeta(struct board_info *board, struct movelist *movelst, int *key, int 
     int i = 0;
     unsigned long long int original_pos = thread_info->CURRENTPOS;
     int movelen = movegen(board, list, color, incheck);
-    movescore(board, list, depth, color, type, depth > 1 ? movelst[*key - 1].move : nullmove, movelen, 0, thread_info);
+    movescore(board, list, depth, color, type, depth > 1 ? movelst[*key - 1].move : nullmove, movelen, 0, thread_info, entry);
     bool raisedalpha = false;
     if (depth == 0)
     {
@@ -467,7 +469,7 @@ int alphabeta(struct board_info *board, struct movelist *movelst, int *key, int 
         if (depth && depth < info.depth * 2)
         { // if we're not already in a singular search, do singular search.
 
-            if (!singularsearch && depthleft >= 7 && list[i].eval == 11000000 && abs(evl) < 50000 && TT[(thread_info->CURRENTPOS) & (_mask)].depth >= depthleft - 3 && type != UBound)
+            if (!singularsearch && depthleft >= 7 && list[i].eval == 11000000 && abs(evl) < 50000 && entry.depth >= depthleft - 3 && type != UBound)
             {
                 int sBeta = ttscore - (depthleft);
 
