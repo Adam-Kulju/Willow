ae076aeb2ce9cfa069ba20d2b410b213a420c27b
Ciekce <kantaja98@gmail.com>
2023-09-22 01:27:18 +0100

    datagen: write data into sequentially numbered files
    and avoid time collisions for seeds in subprocesses

--- src/data.cpp
+++ src/data.cpp
@@ -1,15 +1,20 @@
-using namespace std;
 #include <vector>
 #include <thread>
+#include <string>
 
-void run(){
-    std::system("./datagen");
+void run(int thread_id){
+#ifdef _WIN32
+    std::string command = std::string(".\\datagen.exe data") + std::to_string(thread_id) + ".txt";
+#else
+    std::string command = std::string("./datagen.exe data") + std::to_string(thread_id) + ".txt";
+#endif
+    std::system(command.c_str());
 }
 
 int main(int argc, char *argv[]){
     std::vector<std::thread> threads;
     for (int i = 0; i < atoi(argv[1]); ++i)
-        threads.push_back(std::thread(run));
+        threads.push_back(std::thread(run, i));
 
     // do some other stuff
 
