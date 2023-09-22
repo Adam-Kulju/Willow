a116309111910912bb8f20d5df0a3e14fff22f82
Ciekce <kantaja98@gmail.com>
2023-09-22 01:31:49 +0100

    fix merge conflict

--- src/data.cpp
+++ src/data.cpp
@@ -1,3 +1,7 @@
+#include <vector>
+#include <thread>
+#include <string>
+
 void run(int thread_id){
 #ifdef _WIN32
     std::string command = std::string(".\\datagen.exe data") + std::to_string(thread_id) + ".txt";
@@ -11,7 +15,6 @@ int main(int argc, char *argv[]){
     std::vector<std::thread> threads;
     for (int i = 0; i < atoi(argv[1]); ++i)
         threads.push_back(std::thread(run, i));
-
 
     // do some other stuff
 
